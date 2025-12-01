e1a85e8ebc65c78cd03664fd7452c371fee1ddc1
kiwi0825 <nbeinniwllek@gmail.com>
2025-12-01 14:17:18 -0500

    Fixing Title Page

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,10 +17,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, 250, 150);
+        prepare();
     }
-    
+
     /**
      * The main world act loop
      */
@@ -30,5 +31,29 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        WinnieThePooh winnieThePooh = new WinnieThePooh();
+        addObject(winnieThePooh,480,92);
+        winnieThePooh.setLocation(512,111);
+        winnieThePooh.setLocation(525,201);
+        winnieThePooh.setLocation(486,212);
+        winnieThePooh.setLocation(502,113);
+        winnieThePooh.setLocation(503,127);
+        winnieThePooh.setLocation(496,140);
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,200,288);
+        label.setLocation(312,306);
+        label.setLocation(292,293);
+        Label label2 = new Label("Use <-- and --> to Move", 40);
+        addObject(label2,266,239);
+        label2.setLocation(325,242);
+        label2.setLocation(292,236);
     }
 }
